ea743efa2906fb5ba7c89ea7fe9786dae79d5c96
Akash kumar singh <akashks@iitk.ac.in>
2017-12-29 19:20:20 +0530

    basic

---
+++ script.js
@@ -0,0 +1,21 @@
+class A{
+  constructor(sub,stat,infor){
+    this.sub=sub;
+    this.stat=stat;
+    this.infor=infor;
+  }
+  setsub(sub){
+    this.sub=sub;
+  }
+  setstat(stat){
+    this.stat=stat;
+  }
+  setinfor(infor){
+    this.infor=infor;
+  }
+  show(){
+    
+  }
+}
+var whol=;
+var arr=whol.split('\n');
